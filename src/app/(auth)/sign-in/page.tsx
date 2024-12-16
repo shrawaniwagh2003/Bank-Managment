035a4b3c3b02a75c91e48116b7aa6965986bc306
shrawaniwagh2003 <waghshrawani2003@gmail.com>
2024-12-16 20:25:08 +0530

    modified the sign-in sign-out page with validation with zod

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AuthForm from '@/components/ui/AuthForm'
 
-const Login = () => {
+const SignIn = () => {
   return (
-    <div>Login</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type="sign-in"/>
+    </section>
   )
 }
 
-export default Login
+export default SignIn
